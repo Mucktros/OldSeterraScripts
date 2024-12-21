5dfb5455237da5d84ef402ad02437ead9830c9b6
Mucktros <zanelong76@gmail.com>
2024-12-21 02:49:15 +0100

    Add files via upload

---
+++ Fastclick.js
@@ -0,0 +1,38 @@
+(function () {
+    let isClicking = false;
+
+    function applyFastClick() {
+        const elements = document.querySelectorAll(".q");
+
+        elements.forEach((element) => {
+            element.onmousedown = null;
+            element.onmouseup = null;
+            element.onmousemove = null;
+            element.onclick = null;
+
+            element.addEventListener("mousedown", function (event) {
+                if (event.button === 0 && !isClicking) {
+                    isClicking = true;
+                    event.preventDefault();
+
+                    if (typeof window.checkQuestion === "function") {
+                        window.checkQuestion(this, event);
+                    }
+
+                    setTimeout(() => {
+                        isClicking = false;
+                    }, 50);
+                }
+            });
+        });
+    }
+
+    const observer = new MutationObserver(() => {
+        applyFastClick();
+    });
+
+    const gameContainer = document.body;
+    observer.observe(gameContainer, { childList: true, subtree: true });
+
+    applyFastClick();
+})();
