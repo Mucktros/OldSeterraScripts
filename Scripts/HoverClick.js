7fb3a2515fd17cbce2344e1a3e781bb38157bc36
Mucktros <zanelong76@gmail.com>
2025-02-16 19:37:21 +0100

    Update HoverClick.js

--- Scripts/HoverClick.js
+++ Scripts/HoverClick.js
@@ -5,3 +5,13 @@ function simulateHoverClick() {
 }
 
 simulateHoverClick();
+
+//if you want this to work with other gamemodes so its not just this, you can do something like this
+
+//function simulateFastClick() {
+  //  if (!window.location.search.includes("fastclick=1?shownext=1")) {
+    //    history.replaceState(null, "", window.location.pathname + "fastclick=1?shownext=1");
+    //}
+//}
+
+//simulateFastClick();
