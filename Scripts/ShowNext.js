5afff32cc2d3a2473181942c4c24193dd851627f
Mucktros <zanelong76@gmail.com>
2025-02-16 19:37:36 +0100

    Update ShowNext.js

--- Scripts/ShowNext.js
+++ Scripts/ShowNext.js
@@ -5,3 +5,14 @@ function simulateShowNext() {
 }
 
 simulateShowNext();
+
+
+//if you want this to work with other gamemodes so its not just this, you can do something like this
+
+//function simulateFastClick() {
+  //  if (!window.location.search.includes("fastclick=1?shownext=1")) {
+    //    history.replaceState(null, "", window.location.pathname + "fastclick=1?shownext=1");
+    //}
+//}
+
+//simulateFastClick();
